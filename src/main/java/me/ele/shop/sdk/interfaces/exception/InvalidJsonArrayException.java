f0e80f5cada635acc96255b17e70a6278e69fe99
唐雁 <yan.tang02@ele.me>
2016-12-16 18:23:31 +0800

    modify exception

--- src/main/java/me/ele/shop/sdk/interfaces/exception/InvalidJsonArrayException.java
+++
@@ -1,8 +0,0 @@
-package me.ele.shop.sdk.interfaces.exception;
-
-public class InvalidJsonArrayException extends ServiceException {
-
-    public InvalidJsonArrayException(String property) {
-        super("INVALID_JSON_ARRAY", property + "不是一个有效的JSON数组");
-    }
-}
