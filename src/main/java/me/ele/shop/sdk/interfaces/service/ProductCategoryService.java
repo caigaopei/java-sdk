18b7e9ee2f167089bcc6c0c0a4568f7108aaa9ef
唐雁 <yan.tang02@ele.me>
2016-12-21 16:09:59 +0800

    modify comment and some parameters

--- src/main/java/me/ele/shop/sdk/interfaces/service/ProductCategoryService.java
+++ src/main/java/me/ele/shop/sdk/interfaces/service/ProductCategoryService.java
@@ -1,13 +1,13 @@
 package me.ele.shop.sdk.interfaces.service;
 
 
-
 import me.ele.shop.sdk.anotation.NopService;
 import me.ele.shop.sdk.interfaces.entity.category.OCategory;
 import me.ele.shop.sdk.interfaces.exception.ServiceException;
 
 import javax.validation.constraints.NotNull;
 import java.util.List;
+
 /**
  * eleme.product.category 商品分类服务
  */
@@ -36,20 +36,20 @@ public interface ProductCategoryService {
      *
      * @param shopId      int 店铺Id|必选|987777
      * @param name        String 商品分类名称|必选|"蛋炒饭"
-     * @param description String 商品分类描述|必选|"一道美味的炒饭"
+     * @param description String 商品分类描述|可选|"一道美味的炒饭"
      * @return OCategory 商品分类
      */
-    OCategory createCategory(@NotNull(message = "店铺Id不能为空") Integer shopId, @NotNull(message = "商品分类名称不能为空") String name, @NotNull(message = "商品分类描述不能为空") String description) throws ServiceException;
+    OCategory createCategory(@NotNull(message = "店铺Id不能为空") Integer shopId, @NotNull(message = "商品分类名称不能为空") String name, String description) throws ServiceException;
 
     /**
      * 更新商品分类
      *
      * @param categoryId  long 商品分类Id|必选|26940000135
      * @param name        String 商品分类名称|必选|"蛋炒饭"
-     * @param description String 商品分类描述|必选|"一道美味的炒饭"
+     * @param description String 商品分类描述|可选|"一道美味的炒饭"
      * @return OCategory 商品分类
      */
-    OCategory updateCategory(@NotNull(message = "商品分类Id不能为空") Long categoryId, @NotNull(message = "商品分类名称不能为空") String name, @NotNull(message = "商品分类描述不能为空") String description) throws ServiceException;
+    OCategory updateCategory(@NotNull(message = "商品分类Id不能为空") Long categoryId, @NotNull(message = "商品分类名称不能为空") String name, String description) throws ServiceException;
 
     /**
      * 删除商品分类
