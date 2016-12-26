ce5d074216c18bc111d9249ffa8a483963e34755
唐雁 <yan.tang02@ele.me>
2016-12-26 22:15:58 +0800

    modify MessageService

--- src/main/java/me/ele/shop/sdk/interfaces/service/MessageService.java
+++ src/main/java/me/ele/shop/sdk/interfaces/service/MessageService.java
@@ -22,5 +22,15 @@ public interface MessageService {
      * @return List<OMessage> 消息列表
      * @desc 推送成功的消息系统会给以标记成功，该接口用于获取未标记成功的消息，每次返回最多10条。返回的消息在接口调用后会标记为已经推送。
      */
-    List<OMessage> getNonReachedMessages(@NotNull(message = "应用ID不能为空") Integer appId) throws ServiceException;
+    List<OMessage> getNonReachedOMessages(@NotNull(message = "应用ID不能为空") Integer appId) throws ServiceException;
+
+
+    /**
+     * 获取未到达的推送消息
+     *
+     * @param appId int 应用ID|必选|73987374
+     * @return List<String> 消息列表
+     * @desc 推送成功的消息系统会给以标记成功，该接口用于获取未标记成功的消息，每次返回最多10条。返回的消息在接口调用后会标记为已经推送。
+     */
+    List<String> getNonReachedMessages(@NotNull(message = "应用ID不能为空") Integer appId) throws ServiceException;
 }
