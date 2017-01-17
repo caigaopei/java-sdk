914bcbb044d06c6c869e6ca6ea03ebfc8e71d431
唐雁 <yan.tang02@ele.me>
2017-01-17 11:30:59 +0800

    full oauthutil

--- src/Java/sdk/oauth/ClientCredentials.java
+++ src/Java/sdk/oauth/ClientCredentials.java
@@ -2,14 +2,13 @@ package sdk.oauth;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.http.HttpHeaders;
 import org.apache.log4j.Logger;
 import sdk.config.Config;
 import sdk.util.HttpClientUtil;
-import sun.misc.BASE64Encoder;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
 
 public class ClientCredentials {
 
@@ -26,20 +25,17 @@ public class ClientCredentials {
 
     public Token getAccessToken() {
         try {
-            final String basic = "Basic " + new BASE64Encoder().encode((String.format("%s:%s", Config.getAppKey(), Config.getSecret()).getBytes()));
-            String response = httpClientUtil.post(Config.getAccessTokenUrl(), new HashMap<String, String>() {{
-                put(HttpHeaders.CONTENT_TYPE, "Content-Type: application/x-www-form-urlencoded; charset=utf-8");
-                put(HttpHeaders.AUTHORIZATION, basic);
-            }}, new HashMap<String, String>() {{
-                put("grant_type", "client_credentials");
-            }});
+            Map<String, String> body = new HashMap<String, String>();
+            body.put("grant_type", "client_credentials");
 
-            logger.info(response);
+            String response = httpClientUtil.post(Config.getAccessTokenUrl(), OAuthCommon.getHeaders(), body);
+            logger.info("getAccessToken: " + response);
 
             AccessToken accessToken = objectMapper.readValue(response, AccessToken.class);
             Token token = new Token(accessToken);
             return token;
         } catch (IOException e) {
+            logger.error(e);
             throw new RuntimeException(e);
         }
     }
--- src/Java/sdk/oauth/OAuthUtil.java
+++ src/Java/sdk/oauth/OAuthUtil.java
@@ -1,28 +1,66 @@
 package sdk.oauth;
 
+import org.apache.log4j.Logger;
 import sdk.config.Config;
+import sdk.util.HttpClientUtil;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 public class OAuthUtil {
 
-    public String getAuthUrl(int appId, String redirectUri) throws IOException {
+    Logger logger = Logger.getLogger(this.getClass());
+
+    HttpClientUtil httpClientUtil = new HttpClientUtil();
+
+    public String getAuthUrl(int appId, String redirectUri) {
         String url = Config.getTokenRequestBaseUrl() + "/authorize";
         String responseType = "code";
         String clientId = String.valueOf(appId);
         String state = UUID.randomUUID().toString();
-        String callback = URLEncoder.encode(redirectUri, "utf-8");
+        String callback;
+        try {
+            callback = URLEncoder.encode(redirectUri, "utf-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
         return String.format("%s?response_type=%s&client_id=%s&state=%s&redirect_uri=%s", url, responseType, clientId, state, callback);
     }
 
-    public String getTokenByCode(String code, String redirectUri) {
-        return "";
+    public String getTokenByCode(final String code, String redirectUri) {
+        try {
+            final String callback = URLEncoder.encode(redirectUri, "utf-8");
+            Map<String, String> body = new HashMap<String, String>();
+            body.put("grant_type", "authorization_code");
+            body.put("code", code);
+            body.put("redirect_uri", callback);
+            String response = httpClientUtil.post(Config.getAccessTokenUrl(), OAuthCommon.getHeaders(), body);
+
+            logger.info("getTokenByCode: " + response);
+            return response;
+        } catch (IOException e) {
+            logger.error(e);
+            throw new RuntimeException(e);
+        }
     }
 
     public String getTokenByRefreshToken(String refreshToken) {
-        return "";
+        Map<String, String> body = new HashMap<String, String>();
+        body.put("grant_type", "refresh_token");
+        body.put("refresh_token", refreshToken);
+        String response;
+        try {
+            response = httpClientUtil.post(Config.getAccessTokenUrl(), OAuthCommon.getHeaders(), body);
+        } catch (IOException e) {
+            logger.error(e);
+            throw new RuntimeException(e);
+        }
+        logger.info("getTokenByRefreshToken: " + response);
+        return response;
     }
 
 }
