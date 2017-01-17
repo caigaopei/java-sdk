4ebfc6105f0fc566f8740d4c5e7d2a0ca0b47bc6
唐雁 <yan.tang02@ele.me>
2017-01-17 11:10:52 +0800

    rename

--- src/Java/sdk/oauth/OAuthUtil.java
+++ src/Java/sdk/oauth/OAuthUtil.java
@@ -1,18 +1,19 @@
 package sdk.oauth;
 
 import sdk.config.Config;
-import sun.misc.BASE64Encoder;
 
+import java.io.IOException;
+import java.net.URLEncoder;
 import java.util.UUID;
 
-public class ClientOAuth {
-    
-    public String getAuthUrl(int appId, String redirectUri) {
+public class OAuthUtil {
+
+    public String getAuthUrl(int appId, String redirectUri) throws IOException {
         String url = Config.getTokenRequestBaseUrl() + "/authorize";
         String responseType = "code";
         String clientId = String.valueOf(appId);
         String state = UUID.randomUUID().toString();
-        String callback = new BASE64Encoder().encode(redirectUri.getBytes());
+        String callback = URLEncoder.encode(redirectUri, "utf-8");
         return String.format("%s?response_type=%s&client_id=%s&state=%s&redirect_uri=%s", url, responseType, clientId, state, callback);
     }
 
